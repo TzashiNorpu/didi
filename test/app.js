7cf0bed91355d7d69f9c046cb01bce0d87d486f4
TzashiNorpu <tzashinorpu@gmail.com>
2025-03-29 14:06:44 +0800

    commit

--- test/app.js
+++ test/app.js
@@ -3,6 +3,23 @@ import {expect} from 'chai';
 
 describe('Injector', function() {
   it('should be a function', function() {
+    function Person(name) {
+      this.name = name;
+    }
+    const personModule = {
+      person: ['type', Person]
+    };
+    function Driver() {
+      this.drive = function() {
+        console.log('The driver started driving')
+      }
+    }
+
+    const driverModule = {
+      __depends__: [personModule],
+      driver: ['type', Driver]
+    };
+
     function Car(engine) {
       this.start = function() {
         engine.start();
@@ -19,6 +36,7 @@ describe('Injector', function() {
     // define a (didi) module - it declares available
     // components by name and specifies how these are provided
     const carModule = {
+      __depends__: [driverModule],
 
       // asked for 'car', the injector will call new Car(...) to produce it
       'car': [ 'type', Car ],
